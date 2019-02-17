bf8c3d8a3a28c2bb4abaf7bd321a78f0b0be604a
Manavi <s.manavi@gmail.com>
2019-02-17 11:52:35 +0330

    edit view1

---
+++ app/src/main/java/com/example/myonlinestore1/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.example.myonlinestore1;
+
+import android.os.Bundle;
+
+
+import androidx.annotation.LayoutRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+/**
+ * All activities that host one fragment must extend this class
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    public abstract Fragment createFragment();
+
+    @LayoutRes
+    public abstract int getLayoutResId();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragmentManager.findFragmentById(R.id.fragment_list_container) == null) {
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_list_container, createFragment())
+                    .commit();
+        }
+    }
+}
